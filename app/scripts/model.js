f6d154a0f63797888b4156005c77258d1d098f57
Jonathan Dickerson <jongdickerson@gmail.com>
2014-07-15 14:05:27 -0400

    backbone model and collection

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -1,26 +1,71 @@
 
 
-var Alien = Backbone.Model.extend ({
+// var Alien = Backbone.Model.extend ({
+// 	defaults: {
+// 		name: '',
+// 		location: 'Earth',
+// 		native_planet: 'Mars'
+// 	},
+
+// 	idAttribute: "_id",
+
+// 	initialize: function () {
+// 		var name = this.get('name');
+// 		console.log( name + ' is an Earth transplant.' );
+// 	}
+
+// });
+
+// var ALLaliens = Backbone.Collection.extend ({
+// 	model: Alien, 
+// 	url: "http://tiy-atl-fe-server.herokuapp.com/collections/jon"
+// });
+
+// var all = new ALLaliens(); 
+
+
+var Post = Backbone.Model.extend ({
 	defaults: {
-		name: '',
-		location: 'Earth',
-		native_planet: 'Mars'
+		title: '',
+		content: '',
+		date: '',
+		status: '',
+		author: '',
+		tags: ''
 	},
 
 	idAttribute: "_id",
 
 	initialize: function () {
-		var name = this.get('name');
-		console.log( name + ' is an Earth transplant.' );
+		var title = this.get('title');
+		var content = this.get('content');
+		var date = this.get('date');
+		var status = this.get('status');
+		var author = this.get('author');
+		var taga = this.get('tags');
+		
 	}
 
 });
 
-var ALLaliens = Backbone.Collection.extend ({
-	model: Alien, 
-	url: "http://tiy-atl-fe-server.herokuapp.com/collections/students"
+var ALLposts = Backbone.Collection.extend ({
+	model: Post, 
+	url: "http://tiy-atl-fe-server.herokuapp.com/collections/jblog"
 });
 
-var all = new ALLaliens(); 
+var all = new ALLposts(); 
+
+var post_one = new Post({
+	title: "first post",
+		content: "Content of the post",
+		date: "Today's date",
+		status: "Published status",
+		author: "Jonathan Dickerson",
+		tags: "Generic tag"
+});
+
+all.add(post_one).save();
+
+console.log(post_one);
 
 
